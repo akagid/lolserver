a51eeefe9de1c57272830385dd951aeb5fa2666b
akagi <dev.akagi@gmail.com>
2024-04-02 15:50:36 +0000

    github_tokenを追加

--- main.go
+++ main.go
@@ -10,8 +10,10 @@ func Add(a, b int) int {
 }
 
 func main() {
-	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "pong")
+	http.HandleFunc("/piing", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "poong")
+
+		a := 1
 	})
 
 	http.ListenAndServe(":8081", nil)
