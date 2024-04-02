bd0c27371d4aa8a9ffa4daae7e39e383417872af
akagi <dev.akagi@gmail.com>
2024-04-02 16:25:48 +0000

    指摘修正

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func Add(a, b int) int {
@@ -15,7 +16,13 @@ func main() {
 		fmt.Fprintf(w, "pong")
 	})
 
-	err := http.ListenAndServe(":8080", nil)
+	server := &http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
