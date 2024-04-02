fd4126ca5118964e66f996f61d9238ca73554252
akagi <dev.akagi@gmail.com>
2024-04-02 15:38:02 +0000

    reviewdogをテスト

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ func Add(a, b int) int {
 }
 
 func main() {
-	http.HandleFunc("/piing", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "poong")
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "pong")
 	})
 
 	http.ListenAndServe(":8081", nil)
