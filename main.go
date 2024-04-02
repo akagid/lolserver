2cf93fab14f5e777f2ccc800d2cfb4686e62eb74
akagi <dev.akagi@gmail.com>
2024-04-02 16:09:03 +0000

    指摘修正

--- main.go
+++ main.go
@@ -10,11 +10,9 @@ func Add(a, b int) int {
 }
 
 func main() {
-	http.HandleFunc("/piing", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "poong")
-
-		e := 1
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "pong")
 	})
 
-	http.ListenAndServe(":8081", nil)
+	http.ListenAndServe(":8080", nil)
 }
