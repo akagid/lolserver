d5af5e3850c4cedbe87288378d9d461b5e8af774
akagi <dev.akagi@gmail.com>
2024-04-05 21:00:56 +0000

    サーバー作成処理をテスト

--- main_test.go
+++ main_test.go
@@ -29,3 +29,29 @@ func TestHelloHandler(t *testing.T) {
 		t.Errorf("handler returned unexpected body: got %v want %v", recorder.Body.String(), expected)
 	}
 }
+
+func TestNewServer(t *testing.T) {
+	t.Parallel()
+
+	server := newServer()
+
+	if server.Addr != ":8080" {
+		t.Errorf("server.Addr = %s; want :8080", server.Addr)
+	}
+
+	if server.ReadTimeout != readTimeout {
+		t.Errorf("server.ReadTimeout = %v; want %v", server.ReadTimeout, readTimeout)
+	}
+
+	if server.WriteTimeout != writeTimeout {
+		t.Errorf("server.WriteTimeout = %v; want %v", server.WriteTimeout, writeTimeout)
+	}
+
+	if server.ReadHeaderTimeout != readHeaderTimeout {
+		t.Errorf("server.ReadHeaderTimeout = %v; want %v", server.ReadHeaderTimeout, readHeaderTimeout)
+	}
+
+	if server.IdleTimeout != idleTimeout {
+		t.Errorf("server.IdleTimeout = %v; want %v", server.IdleTimeout, idleTimeout)
+	}
+}
