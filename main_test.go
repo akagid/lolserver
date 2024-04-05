5f079f1187abe9560ff626e5acd7f6d0efeffd2a
akagi <dev.akagi@gmail.com>
2024-04-05 19:51:11 +0000

    レコーダーの名前を変更

--- main_test.go
+++ main_test.go
@@ -15,17 +15,17 @@ func TestHelloHandler(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	rr := httptest.NewRecorder()
+	recorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(helloHandler)
 
-	handler.ServeHTTP(rr, req)
+	handler.ServeHTTP(recorder, req)
 
-	if status := rr.Code; status != http.StatusOK {
+	if status := recorder.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
 
 	expected := "Hello, World!"
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	if recorder.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", recorder.Body.String(), expected)
 	}
 }
