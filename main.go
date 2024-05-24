91929c2c8ec4f4445d72861749fffad585e0cd1f
akagi <dev.akagi@gmail.com>
2024-05-24 06:36:52 +0000

    サーバーの実装を初期化

--- main.go
+++ main.go
@@ -1,25 +1,13 @@
 package main
 
-import (
-	"log"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
-func setupRouter() *gin.Engine {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
-
-	return r
-}
+import "github.com/gin-gonic/gin"
 
 func main() {
-	r := setupRouter()
-
-	if err := r.Run(":8080"); err != nil {
-		log.Fatalf("Failed to run server: %v", err)
-	}
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
